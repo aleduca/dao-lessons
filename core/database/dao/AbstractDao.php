9a3470fd247b41574e33de85a7dffe9966dd6856
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2025-07-24 16:19:07 -0300

    finders

--- core/database/dao/AbstractDao.php
+++ core/database/dao/AbstractDao.php
@@ -15,4 +15,26 @@ abstract class AbstractDao
   {
     $this->connection = Connection::getConnection();
   }
+
+  public function findAll(string $fields = '*'): array
+  {
+    $sql = "SELECT {$fields} from {$this->table}";
+    $select = $this->connection->query($sql);
+    return $select->fetchAll();
+  }
+
+  public function findBy(string $field, mixed $value, string $fields = '*'): array
+  {
+    $sql = "SELECT {$fields} from {$this->table} where {$field} = :{$field}";
+    $prepare = $this->connection->prepare($sql);
+    $prepare->execute([
+      $field => $value
+    ]);
+    return $prepare->fetch();
+  }
+
+  public function findById(int $id, string $fields = '*')
+  {
+    return $this->findBy('id', $id, $fields);
+  }
 }
